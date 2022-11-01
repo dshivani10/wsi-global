ffd540ebcb630adc59cd241464b53b688a2bd53d
sdevasani <sdevasani@nisum.com>
2022-11-01 12:45:13 +0530

    initial setup

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="mt-10 text-3xl mx-auto max-w-6xl">
+    <Header/>
+    <div>Global MFE</div>
+  </div>
+</template>
+<script>
+import Header from "./Header.vue";
+
+export default{
+  components:{
+    Header,
+  }
+}
+</script>
